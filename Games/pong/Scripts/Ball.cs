0e5f44cb3307b719450872a47e749a42453dc1f4
Jakitybob <jakitybob@gmail.com>
2025-03-11 21:44:22 -0500

    Implemented ball physics
    Using a RigidBody2D, the ball moves in a random direction initially and proceeds to bounce around at the given speed until it flies off the map.

--- Games/pong/Scripts/Ball.cs
+++ Games/pong/Scripts/Ball.cs
@@ -1,21 +1,60 @@
 using Godot;
 using System;
+using System.Linq.Expressions;
+using System.Threading;
 
 public partial class Ball : RigidBody2D
 {
     [Export] float speed = 200f;
 
+    private Vector2 direction;
+
     public override void _Ready()
     {
-        // Activate contact monitoring
-        ContactMonitor = true;
-        MaxContactsReported = 5;
+        // Disable gravity
+        GravityScale = 0;
 
-        LinearVelocity = new Vector2(-250f, 0); // Make this a random direction towards either side
+        // Activate monitoring
+        ContactMonitor = true;
+
+        // Set ball off with base direction
+        direction = GenerateRandomDirection();
     }
 
-    public void OnBodyEntered(Node body)
+    public override void _PhysicsProcess(double delta)
     {
-        GD.Print("Boop!");
+        // Move the ball and check for collisions
+        KinematicCollision2D collision = MoveAndCollide(direction.Normalized() * speed * (float)delta);
+
+        // If a collision occurred
+        if (collision != null)
+        {
+            Vector2 reflect = collision.GetRemainder().Reflect(collision.GetNormal()); // Get the immediate reflection of the ball from the surface
+            direction = direction.Bounce(collision.GetNormal()) * (float)delta; // Get the new direction based on the bounce
+            MoveAndCollide(reflect); // Move the ball once in the direction of the reflection
+        }
+    }
+
+    private Vector2 GenerateRandomDirection()
+    {
+        // Create the direction vector to return later
+        Vector2 newDirection;
+
+        // Generate the angle from -45 to 45 degrees
+        float angle = (float)GD.RandRange(-1f, 1f);
+
+        // Set the direction to the left if random number is even
+        if (GD.Randi() % 2 == 0)
+        {
+            newDirection = Vector2.Left;
+        }
+        // Else set the direction to right if the number is odd
+        else
+        {
+            newDirection = Vector2.Right;
+        }
+
+        newDirection.Y = angle;
+        return newDirection.Normalized();
     }
 }
--- Games/pong/Scripts/Paddle.cs
+++ Games/pong/Scripts/Paddle.cs
@@ -17,9 +17,10 @@ public partial class Paddle : StaticBody2D
             Position += new Vector2(0, (float)(speed * delta));
         }
 
-        // Clamp paddle to bounds of screen
-        // TODO: actually clamp with the paddle size in mind :)
-        Vector2 clampedPosition = new Vector2(Position.X, Mathf.Clamp(Position.Y, 0, GetViewportRect().Size.Y));
+        // Clamp paddle to bounds of screen with paddle and wall size in mind
+        float height = GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size.Y;
+        float wallHeight = GetTree().CurrentScene.GetNode<StaticBody2D>("Wall").GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size.Y;
+        Vector2 clampedPosition = new Vector2(Position.X, Mathf.Clamp(Position.Y, 0 + height / 2 + wallHeight, GetViewportRect().Size.Y - height/2));
         Position = clampedPosition;
     }
 }
