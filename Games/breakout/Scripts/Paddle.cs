7783d861a0f0c71db1f59ed5a4257679815e01b9
Jakitybob <jakitybob@gmail.com>
2025-03-13 19:36:12 -0500

    Initial breakout commit
    Created basic project files, modified project settings, and ported over some applicable code from Pong project

---
+++ Games/breakout/Scripts/Paddle.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Paddle : StaticBody2D
+{
+    [Export] float speed = 350f; // Arbitrary value idk man
+
+    public override void _Process(double delta)
+    {
+        HandleInput((float)delta);
+
+        // Clamp paddle to bounds of screen with paddle and wall size in mind
+        float width = GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size.X;
+        float wallWidth = 0; //GetTree().CurrentScene.GetNode<StaticBody2D>("Wall").GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size.X;
+        Vector2 clampedPosition = new Vector2(Mathf.Clamp(Position.X, 0 + width / 2 + wallWidth, GetViewportRect().Size.X - width/2 - wallWidth), Position.Y);
+        Position = clampedPosition;
+    }
+
+    private void HandleInput(float delta)
+    {
+        if (Input.IsActionPressed("escape")) // Should be done in an input manager but I'm doing it here instead :^)
+        {
+            //GameController.instance.EndGame();
+        }
+
+        if (Input.IsActionPressed("left"))
+        {
+            Position -= new Vector2(speed * delta, 0);
+        }
+        if (Input.IsActionPressed("right"))
+        {
+            Position += new Vector2(speed * delta, 0);
+        }
+    }
+}
